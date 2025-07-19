d834e218b9c6538b916dfa66d1f8fdd736608a09
Bryon2 <111780565+Bryon2@users.noreply.github.com>
2025-07-19 11:13:30 -0400

    fixed oreder of opperations and corrected the third number to read n3 instead of n2

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	// Convert the sum to float by dividing with a float value
+	// Added () around the sum calculation for order of op
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
@@ -13,8 +15,9 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
-
+	cout << "Third number: "; cin >> n3; // was originaly n2 and was not reading n3
+	
+	// Call the average function with the three inputs
 	float a = average(n1, n2, n3);
 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
